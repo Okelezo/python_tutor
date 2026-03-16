f9cf8d7c41f515fc47996483a17c6c5ec29f7070
voke <oo411@bath.ac.uk>
2026-03-16 17:48:22 +0000

    changed UI scheme  and structure

--- src/pytutor/engine.py
+++ src/pytutor/engine.py
@@ -34,19 +34,17 @@ def grade_submission(user_code: str, tests_code: str) -> GradeResult:
     stderr = io.StringIO()
 
     try:
-        globals_ns: dict[str, object] = {
+        ns: dict[str, object] = {
             "__name__": "__pytutor__",
             "unittest": unittest,
             "_UserCodeTestCase": _UserCodeTestCase,
         }
-        locals_ns: dict[str, object] = {}
 
         with redirect_stdout(stdout), redirect_stderr(stderr):
-            exec(compile(user_code, "<user_code>", "exec"), globals_ns, locals_ns)
-            exec(compile(tests_code, "<tests>", "exec"), globals_ns, locals_ns)
+            exec(compile(user_code, "<user_code>", "exec"), ns, ns)
+            exec(compile(tests_code, "<tests>", "exec"), ns, ns)
 
-            combined_ns = {**globals_ns, **locals_ns}
-            case_classes = _discover_testcase_classes(combined_ns)
+            case_classes = _discover_testcase_classes(ns)
             suite = unittest.TestSuite()
             for case_cls in case_classes:
                 suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(case_cls))
